0e42e101144a5f8a10a015b859f80c43eac9d7b2
Antonio Zanatta <antoniogsztt@gmail.com>
2024-08-12 17:40:14 +0000

    estrutura do projeto e arquivos

---
+++ .gradle/src/main/java/application/Application.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@springBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(PrimarySource:Application.class, args);
+    }
+}
